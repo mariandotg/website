3dd65f449589c0553fcbde305b2b76dbbf756a62
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-08-14 19:07:37 -0300

    Added prefixDefaultLocale true config to astroConfig

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,6 +19,9 @@ export default defineConfig({
   }),],
   i18n: {
     defaultLocale,
-    locales: localesArray
+    locales: localesArray,
+    routing: {
+      prefixDefaultLocale: true
+    }
   }
 });
