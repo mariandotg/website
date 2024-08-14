9607e494fb68e6fef1bb3cb966d698648b8f2252
Mariano Guillaume <marianguillaume.m@gmail.com>
2024-08-14 19:09:20 -0300

    Fixed astroConfig site string

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://cv.marianoguillaume.com',
+  site: 'https://marianoguillaume.com',
   integrations: [tailwind(), react(), sitemap(), partytown({
     config: {
       forward: ["dataLayer.push"],
